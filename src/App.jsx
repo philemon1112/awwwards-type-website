ed1efa6e450a20447de8ab0dba41e287ad36eb4b
Philemon Abakah <philemon.abakah@hubtel.com>
2025-08-15 01:08:21 +0000

    feat: added feature sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import Hero from "./components/Hero"
+import Navbar from "./components/Navbar"
+import About from "./sections/About"
+import Features from "./sections/Features"
+import Hero from "./sections/Hero"
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Navbar />
       <Hero />
-
-      <section className="z-0 min-h-screen bg-black"></section>
+      <About />
+      <Features />
     </main>
   )
 }
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,8 +4,8 @@ import { ScrollTrigger } from "gsap/all";
 import { TiLocationArrow } from "react-icons/ti";
 import { useEffect, useRef, useState } from "react";
 
-import Button from "./Button";
-import VideoPreview from "./VideoPreview";
+import Button from "../components/Button";
+import VideoPreview from "../components/VideoPreview";
 
 gsap.registerPlugin(ScrollTrigger);
 
